ff5bf20b56fe3dacc924d6f5f328fff8d69c7861
Kevin Arehart <kevinarehart@gmail.com>
2017-08-01 14:46:42 -0500

    initial commit

---
+++ corrective-exercise/main.py
@@ -0,0 +1,15 @@
+import webapp2, jinja2, os, re
+from google.appengine.ext import db
+#edit these in models.py - from models import Post, User
+import hashutils
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
