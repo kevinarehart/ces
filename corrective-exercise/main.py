b6b52be2e87d7e404d1ae01299ee08c8cf203e3b
Kevin Arehart <kevinarehart@gmail.com>
2017-08-01 15:41:50 -0500

    set up handlers in main.py

--- corrective-exercise/main.py
+++ corrective-exercise/main.py
@@ -1,6 +1,6 @@
 import webapp2, jinja2, os, re
 from google.appengine.ext import db
-#edit these in models.py - from models import Post, User
+from models import User
 import hashutils
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -10,6 +10,36 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class GoldenCircleHandler(MainHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+class ProgramsHandler(MainHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+class DiagnosticsHandler(MainHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+class ExercisesHandler(MainHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+class LoginHandler(MainHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+class LogoutHandler(MainHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/goldencircle', GoldenCircleHandler),
+    ('/programs', ProgramsHandler),
+    ('/diagnostics', DiagnosticsHandler),
+    ('/exercises', ExercisesHandler),
+    ('/login', LoginHandler),
+    ('/logout', LogoutHandler)
 ], debug=True)
